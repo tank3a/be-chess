c1e875403a4bf2cdb0b0615f23d9ae84a1598bf5
Hoyoung Jung <honux77@gmail.com>
2023-06-30 14:13:46 +0900

    테스트코드 추가

--- src/test/java/softeer2nd/HelloTest.java
+++ src/test/java/softeer2nd/HelloTest.java
@@ -1,4 +1,16 @@
-import static org.junit.jupiter.api.Assertions.*;
+package softeer2nd;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 class HelloTest {
-  
+
+    @Test
+    @DisplayName("간단 테스트")
+    void hello() {
+        int a = 777;
+        assertThat(a).isEqualTo(777);
+    }
 }
