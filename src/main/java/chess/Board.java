e18da3ba51a2a52bb22a6a3285027a220fa27a54
tank3a <tank3a@gmail.com>
2023-07-06 11:54:34 +0900

    rank 클래스를 별도로 만들어 관리(배열에서 list로 변경)

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,11 +1,9 @@
 package chess;
 
 import chess.pieces.Piece;
-import chess.pieces.Piece.Color;
-import chess.pieces.Piece.Type;
+import chess.pieces.Rank;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 import static chess.Position.convertXIndex;
@@ -14,77 +12,25 @@ import static chess.StringUtils.appendNewLine;
 
 public class Board {
 
-    class Rank {
-        Piece[] rowList = new Piece[8];
-
-        @Override
-        public String toString() {
-            StringBuilder rank = new StringBuilder();
-            Arrays.stream(rowList).forEach(piece -> rank.append(piece.getTypeInCharacter()));
-
-            return rank.toString();
-        }
-    }
-
     private List<Rank> boardList;
+    private final int BOARD_SIZE = 64;
 
 
     public void initialize() {
         initializeEmpty();
-        insertInitialBlackOthers();
-        insertInitialBlackPawn();
-        insertInitialWhiteOthers();
-        insertInitialWhitePawn();
+
+        boardList.get(0).setInitialBlackOther();
+        boardList.get(1).setInitialBlackPawn();
+        boardList.get(6).setInitialWhitePawn();
+        boardList.get(7).setInitialWhiteOther();
     }
 
     public void initializeEmpty() {
-        boardList = new ArrayList<>() {{
-            add(new Rank());
-            add(new Rank());
-            add(new Rank());
-            add(new Rank());
-            add(new Rank());
-            add(new Rank());
-            add(new Rank());
-            add(new Rank());
-        }};
-        boardList.stream().forEach(rank -> Arrays.fill(rank.rowList, Piece.createBlank()));
-    }
+        boardList = new ArrayList<>();
 
-    private void insertInitialBlackPawn() {
-        for(int index = 0; index < 8; index++) {
-            boardList.get(1).rowList[index] = Piece.createBlackPawn();
+        for (int index = 0; index < 8; index++) {
+            boardList.add(new Rank());
         }
-    }
-
-    private void insertInitialWhitePawn() {
-        for(int index = 0; index < 8; index++) {
-            boardList.get(6).rowList[index] = Piece.createWhitePawn();
-        }
-    }
-
-    private void insertInitialBlackOthers() {
-        Rank rank = boardList.get(0);
-        rank.rowList[0] = Piece.createBlackRook();
-        rank.rowList[7] = Piece.createBlackRook();
-        rank.rowList[1] = Piece.createBlackKnight();
-        rank.rowList[6] = Piece.createBlackKnight();
-        rank.rowList[2] = Piece.createBlackBishop();
-        rank.rowList[5] = Piece.createBlackBishop();
-        rank.rowList[3] = Piece.createBlackQueen();
-        rank.rowList[4] = Piece.createBlackKing();
-    }
-
-    private void insertInitialWhiteOthers() {
-        Rank rank = boardList.get(7);
-        rank.rowList[0] = Piece.createWhiteRook();
-        rank.rowList[7] = Piece.createWhiteRook();
-        rank.rowList[1] = Piece.createWhiteKnight();
-        rank.rowList[6] = Piece.createWhiteKnight();
-        rank.rowList[2] = Piece.createWhiteBishop();
-        rank.rowList[5] = Piece.createWhiteBishop();
-        rank.rowList[3] = Piece.createWhiteQueen();
-        rank.rowList[4] = Piece.createWhiteKing();
     }
 
     public String showBoard() {
@@ -92,15 +38,7 @@ public class Board {
     }
 
     public int countAll() {
-        int count = 0;
-        for(Rank rank : boardList) {
-            for (Piece piece : rank.rowList) {
-                if(piece.isWhite() || piece.isBlack())
-                count++;
-            }
-        }
-
-        return count;
+        return BOARD_SIZE - getPieceCount(Piece.createBlank());
     }
 
     @Override
@@ -111,24 +49,19 @@ public class Board {
         return board.toString();
     }
 
-    public int getPieceCount(Piece pieceToCompare) {
+    public int getPieceCount(Piece pieceToCount) {
         int count = 0;
         for (Rank rank : boardList) {
-            for (Piece piece : rank.rowList) {
-                if(piece.equals(pieceToCompare)) {
-                    count++;
-                }
-            }
+            count += rank.countPiece(pieceToCount);
         }
-
         return count;
     }
 
     public Piece findPiece(String position) {
-        return boardList.get(convertYIndex(position)).rowList[convertXIndex(position)];
+        return boardList.get(convertYIndex(position)).getPiece(convertXIndex(position));
     }
 
     public void move(String position, Piece pieceToMove) {
-        boardList.get(convertYIndex(position)).rowList[convertXIndex(position)] = pieceToMove;
+        boardList.get(convertYIndex(position)).setPiece(convertXIndex(position), pieceToMove);
     }
 }
---
+++ src/main/java/chess/pieces/Rank.java
@@ -0,0 +1,75 @@
+package chess.pieces;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static chess.StringUtils.appendNewLine;
+
+public class Rank {
+    List<Piece> rank;
+
+    public Rank() {
+        rank = new ArrayList<>();
+        for(int repeat = 0; repeat < 8; repeat++) {
+            rank.add(Piece.createBlank());
+        }
+    }
+
+    public void setInitialBlackPawn() {
+        rank.clear();
+        for(int index = 0; index < 8; index++) {
+            rank.add(Piece.createBlackPawn());
+        }
+    }
+
+    public void setInitialWhitePawn() {
+        rank.clear();
+        for(int index = 0; index < 8; index++) {
+            rank.add(Piece.createWhitePawn());
+        }
+    }
+
+    public void setInitialBlackOther() {
+        rank.clear();
+        rank.add(Piece.createBlackRook());
+        rank.add(Piece.createBlackKnight());
+        rank.add(Piece.createBlackBishop());
+        rank.add(Piece.createBlackQueen());
+        rank.add(Piece.createBlackKing());
+        rank.add(Piece.createBlackBishop());
+        rank.add(Piece.createBlackKnight());
+        rank.add(Piece.createBlackRook());
+    }
+
+    public void setInitialWhiteOther() {
+        rank.clear();
+        rank.add(Piece.createWhiteRook());
+        rank.add(Piece.createWhiteKnight());
+        rank.add(Piece.createWhiteBishop());
+        rank.add(Piece.createWhiteQueen());
+        rank.add(Piece.createWhiteKing());
+        rank.add(Piece.createWhiteBishop());
+        rank.add(Piece.createWhiteKnight());
+        rank.add(Piece.createWhiteRook());
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        rank.stream().forEach(piece -> stringBuilder.append(piece.getTypeInCharacter()));
+
+        return stringBuilder.toString();
+    }
+
+    public int countPiece(Piece pieceToCount) {
+        return (int) rank.stream().filter(piece -> piece.equals(pieceToCount)).count();
+    }
+
+    public Piece getPiece(int index) {
+        return rank.get(index);
+    }
+
+    public void setPiece(int index, Piece pieceToSet) {
+        rank.set(index, pieceToSet);
+    }
+}
